202fe5a6227e67cbfa75b3b12681190aeed7a445
Hiroshi Hayakawa <hhiroshell@gmail.com>
2019-11-28 14:58:46 +0900

    Initial commit.

---
+++ src/main/java/com/github/cndjp/godfather/GodfatherException.java
@@ -0,0 +1,9 @@
+package com.github.cndjp.godfather;
+
+public class GodfatherException extends Exception {
+
+    public GodfatherException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/github/cndjp/godfather/renderer/GodfatherRendererException.java
@@ -0,0 +1,11 @@
+package com.github.cndjp.godfather.renderer;
+
+import com.github.cndjp.godfather.GodfatherException;
+
+public class GodfatherRendererException extends GodfatherException {
+
+    GodfatherRendererException(String message) {
+        super(message);
+    }
+
+}
