180c7ef53570b161e214e562b96e5b8136349ed4
nnao45 <n4sekai5y@gmail.com>
2020-01-12 18:08:06 +0900

    getParticipantsを作成した

--- src/main/java/com/github/cndjp/godfather/domain/event/ConnpassEvent.scala
+++ src/main/java/com/github/cndjp/godfather/domain/event/ConnpassEvent.scala
@@ -2,4 +2,10 @@ package com.github.cndjp.godfather.domain.event
 
 import java.net.URL
 
-case class ConnpassEvent(url: URL) extends Event
+case class ConnpassEvent(url: URL) extends Event {
+  def getParticipantsListUrl: String = {
+    val input = this.url.toString
+    val suffix = if (input.endsWith("/")) "participation/" else  "/participation/";
+    input + suffix
+  }
+}
--- src/main/java/com/github/cndjp/godfather/domain/participant/ConnpassParticipant.scala
+++ src/main/java/com/github/cndjp/godfather/domain/participant/ConnpassParticipant.scala
@@ -2,4 +2,5 @@ package com.github.cndjp.godfather.domain.participant
 
 import java.net.URL
 
-case class ConnpassParticipant(id: Long, name: String, imageURL: URL, status: ParticipantStatus) extends Participant
+case class ConnpassParticipant(id: String, name: String, imageURL: URL, status: ParticipantStatus)
+    extends Participant
