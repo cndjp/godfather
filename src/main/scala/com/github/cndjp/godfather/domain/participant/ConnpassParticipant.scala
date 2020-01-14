3b306b8e21b033fd9c897386e21d1b36e32b5bd9
nnao45 <n4sekai5y@gmail.com>
2020-01-14 22:53:15 +0900

    ええ感じやで

--- src/main/scala/com/github/cndjp/godfather/domain/participant/ConnpassParticipant.scala
+++ src/main/scala/com/github/cndjp/godfather/domain/participant/ConnpassParticipant.scala
@@ -1,7 +1,31 @@
 package com.github.cndjp.godfather.domain.participant
 
 import java.net.URL
+import java.util.UUID
+import org.jsoup.nodes.{Document, Element}
 
 // connpassイベントに参加する人を表すドメインクラス
-case class ConnpassParticipant(id: String, name: String, imageURL: URL, status: ParticipantStatus)
-    extends Participant
+case class ConnpassParticipant(id: String, name: String, imageURL: URL) extends Participant
+
+object ConnpassParticipant {
+  private[this] val IMAGE_SOURCE_DEFAULT = new URL(
+    "https://connpass.com/static/img/common/user_no_image_180.png")
+
+  def apply(displayName: String, userDoc: Document): ConnpassParticipant = {
+    val images = userDoc.select("div[id=side_area] div[class=mb_20 text_center] a.image_link")
+    val imageSource =
+      if (!images.isEmpty)
+        images
+          .toArray(Array[Element]())
+          .find(_.attr("href")
+            .contains("/user/"))
+          .map(image => new URL(image.attr("href")))
+          .getOrElse(IMAGE_SOURCE_DEFAULT)
+      else IMAGE_SOURCE_DEFAULT
+    ConnpassParticipant(
+      UUID.randomUUID().toString,
+      displayName,
+      imageSource,
+    )
+  }
+}
