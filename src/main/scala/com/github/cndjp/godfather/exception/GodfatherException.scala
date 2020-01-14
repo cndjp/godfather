1c4fc30e57abbbd95e2c3bda54426a96500dc8f6
nnao45 <n4sekai5y@gmail.com>
2020-01-14 11:23:44 +0900

    ちゃんと例外気候

--- src/main/scala/com/github/cndjp/godfather/exception/GodfatherException.scala
+++ src/main/scala/com/github/cndjp/godfather/exception/GodfatherException.scala
@@ -4,14 +4,8 @@ sealed abstract class GodfatherException(why: String) extends Exception(why)
 
 object GodfatherException {
   case class GodfatherGeneralException(why: String)
-    extends GodfatherException(s"予測されないエラーが発生しました: $why")
-
-  case class GodfatherEventException(why: String)
-    extends GodfatherException(s"不正なイベントURLが検出されました: $why")
-
-  case class GodfatherPreviewException(why: String)
-    extends GodfatherException(s"プレビューサーバの処理で不正イベントを検知しました: $why")
+      extends GodfatherException(s"予測されないエラーが発生しました: $why")
 
   case class GodfatherRendererException(why: String)
-    extends GodfatherException(s"レンダリングに失敗しました: $why")
+      extends GodfatherException(s"レンダリングに失敗しました: $why")
 }
