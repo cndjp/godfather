799ccd60c9aced514d177f5072556b717c44256c
nnao45 <n4sekai5y@gmail.com>
2020-01-14 16:09:06 +0900

    こっちが安定だな

--- src/main/scala/com/github/cndjp/godfather/usecase/RenderUsecaseImpl.scala
+++ src/main/scala/com/github/cndjp/godfather/usecase/RenderUsecaseImpl.scala
@@ -7,6 +7,7 @@ import com.github.cndjp.godfather.domain.event.ConnpassEvent
 import com.github.cndjp.godfather.domain.repository.ConnpassEventRepository
 import cats.effect.implicits._
 import cats.syntax.all._
+import com.github.cndjp.godfather.exception.GodfatherException.GodfatherGeneralException
 import com.typesafe.scalalogging.LazyLogging
 
 import scala.io.Source
@@ -16,20 +17,15 @@ class RenderUsecaseImpl(connpassEventRepository: ConnpassEventRepository)
     with LazyLogging {
   private[this] val resourcesPath = "./src/main/resources"
 
-  override def exec(event: ConnpassEvent): IO[String] =
+  override def exec(event: ConnpassEvent): IO[Unit] =
     for {
       cardHTMLPath <- IO(Paths.get(s"$resourcesPath/cards.html"))
       _ <- if (Files.exists(cardHTMLPath)) IO.unit else render(event, cardHTMLPath)
-      indexHTML <- Resource
-                    .fromAutoCloseable(IO(Source.fromFile(s"$resourcesPath/index.html")))
-                    .use { file =>
-                      IO(file.mkString)
-                    }
-    } yield indexHTML
+    } yield ()
 
   private[this] def render(event: ConnpassEvent, cardHTMLPath: Path): IO[Unit] =
     for {
-      cardHTML <- IO { Files.createFile(cardHTMLPath) }
+      cardHTML <- IO(Files.createFile(cardHTMLPath))
       participants <- connpassEventRepository.getParticipants(event)
       title <- connpassEventRepository.getEventTitle(event)
       output <- connpassEventRepository.participantList2String(title, participants)
@@ -38,6 +34,7 @@ class RenderUsecaseImpl(connpassEventRepository: ConnpassEventRepository)
             .use { pw =>
               IO(pw.write(output))
             }
+            .handleErrorWith(e => IO.raiseError(GodfatherGeneralException(e.getMessage)))
       _ <- IO(logger.info("Finish for rendering!!"))
     } yield ()
 }
