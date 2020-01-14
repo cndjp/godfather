16d994ec25c8d9563e8a072a221fb00132b12a26
nnao45 <n4sekai5y@gmail.com>
2020-01-14 15:44:39 +0900

    リソースさよなら

--- src/main/scala/com/github/cndjp/godfather/usecase/RenderUsecaseImpl.scala
+++ src/main/scala/com/github/cndjp/godfather/usecase/RenderUsecaseImpl.scala
@@ -2,21 +2,29 @@ package com.github.cndjp.godfather.usecase
 import java.io.PrintWriter
 import java.nio.file.{Files, Path, Paths}
 
-import cats.implicits._
-import cats.effect.IO
+import cats.effect.{IO, Resource}
 import com.github.cndjp.godfather.domain.event.ConnpassEvent
 import com.github.cndjp.godfather.domain.repository.ConnpassEventRepository
+import cats.effect.implicits._
+import cats.syntax.all._
+import com.typesafe.scalalogging.LazyLogging
+
 import scala.io.Source
 
-class RenderUsecaseImpl(connpassEventRepository: ConnpassEventRepository) extends RenderUsecase {
+class RenderUsecaseImpl(connpassEventRepository: ConnpassEventRepository)
+    extends RenderUsecase
+    with LazyLogging {
   private[this] val resourcesPath = "./src/main/resources"
 
   override def exec(event: ConnpassEvent): IO[String] =
     for {
       cardHTMLPath <- IO(Paths.get(s"$resourcesPath/cards.html"))
       _ <- if (Files.exists(cardHTMLPath)) IO.unit else render(event, cardHTMLPath)
-      indexHTML <- IO(Source.fromFile(s"$resourcesPath/index.html"))
-                    .bracket(file => IO(file.mkString))(file => IO(file.close()))
+      indexHTML <- Resource
+                    .fromAutoCloseable(IO(Source.fromFile(s"$resourcesPath/index.html")))
+                    .use { file =>
+                      IO(file.mkString)
+                    }
     } yield indexHTML
 
   private[this] def render(event: ConnpassEvent, cardHTMLPath: Path): IO[Unit] =
@@ -25,7 +33,11 @@ class RenderUsecaseImpl(connpassEventRepository: ConnpassEventRepository) extend
       participants <- connpassEventRepository.getParticipants(event)
       title <- connpassEventRepository.getEventTitle(event)
       output <- connpassEventRepository.participantList2String(title, participants)
-      _ <- IO(new PrintWriter(cardHTML.toFile.getPath))
-            .bracket(pw => IO(pw.write(output)))(pw => IO(pw.close())) *> IO.unit
+      _ <- Resource
+            .fromAutoCloseable(IO(new PrintWriter(cardHTML.toFile.getPath)))
+            .use { pw =>
+              IO(pw.write(output))
+            }
+      _ <- IO(logger.info("Finish for rendering!!"))
     } yield ()
 }
