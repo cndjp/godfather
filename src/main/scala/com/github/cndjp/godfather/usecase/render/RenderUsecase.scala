4b04acc6b170d97c1fef96c8beeabf9ff1575766
nnao45 <n4sekai5y@gmail.com>
2020-01-16 01:09:12 +0900

    usecaseそうのテスト書き終えた2

--- src/main/scala/com/github/cndjp/godfather/usecase/render/RenderUsecase.scala
+++ src/main/scala/com/github/cndjp/godfather/usecase/render/RenderUsecase.scala
@@ -2,7 +2,6 @@ package com.github.cndjp.godfather.usecase.render
 
 import cats.effect.IO
 import com.github.cndjp.godfather.domain.event.ConnpassEvent
-import com.github.cndjp.godfather.domain.resourcePath.ResourcePath
 import com.twitter.io.Buf
 
 trait RenderUsecase {
