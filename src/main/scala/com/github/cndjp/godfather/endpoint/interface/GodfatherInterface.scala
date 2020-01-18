d68a045c2b18ff2249a8a99f261611693a1221f9
nnao45 <n4sekai5y@gmail.com>
2020-01-18 16:50:29 +0900

    ScrapeAdapterはインフラそう配下かな

--- src/main/scala/com/github/cndjp/godfather/endpoint/interface/GodfatherInterface.scala
+++ src/main/scala/com/github/cndjp/godfather/endpoint/interface/GodfatherInterface.scala
@@ -1,9 +1,8 @@
 package com.github.cndjp.godfather.endpoint.interface
 
-import com.github.cndjp.godfather.domain.adapter.scrape.ScrapeAdapter
 import com.github.cndjp.godfather.domain.repository.event.ConnpassEventRepository
 import com.github.cndjp.godfather.domain.repository.participant.ConnpassParticipantRepository
-import com.github.cndjp.godfather.infrastructure.adapter.scrape.ScrapeAdapterImpl
+import com.github.cndjp.godfather.infrastructure.adapter.scrape.{ScrapeAdapter, ScrapeAdapterImpl}
 import com.github.cndjp.godfather.infrastructure.repository.event.ConnpassEventRepositoryImpl
 import com.github.cndjp.godfather.infrastructure.repository.participant.ConnpassParticipantRepositoryImpl
 import com.github.cndjp.godfather.usecase.render.{RenderUsecase, RenderUsecaseImpl}
