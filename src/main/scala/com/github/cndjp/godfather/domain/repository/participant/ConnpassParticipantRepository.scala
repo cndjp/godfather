c524e9ad87485e9574c1309da79f002625a93925
nnao45 <n4sekai5y@gmail.com>
2020-01-20 16:02:26 +0900

    わかりやすい値クラスに

--- src/main/scala/com/github/cndjp/godfather/domain/repository/participant/ConnpassParticipantRepository.scala
+++ src/main/scala/com/github/cndjp/godfather/domain/repository/participant/ConnpassParticipantRepository.scala
@@ -1,11 +1,11 @@
 package com.github.cndjp.godfather.domain.repository.participant
 
 import cats.effect.IO
-import com.github.cndjp.godfather.domain.cards.Cards
+import com.github.cndjp.godfather.domain.cards.RenderedCards
 import com.github.cndjp.godfather.domain.participant.{ConnpassParticipant, ParticipantStatus}
 import org.jsoup.select.Elements
 
 trait ConnpassParticipantRepository {
-  def parseParticipantList(title: String, input: Seq[ConnpassParticipant]): IO[Cards]
+  def parseParticipantList(title: String, input: Seq[ConnpassParticipant]): IO[RenderedCards]
   def element2Participants(input: Seq[(ParticipantStatus, Elements)]): IO[Seq[ConnpassParticipant]]
 }
