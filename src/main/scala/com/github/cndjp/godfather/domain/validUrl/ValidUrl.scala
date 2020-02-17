a54e98e57d6a22aad20de8b59c247f7c8d3377f3
nnao45 <n4sekai5y@gmail.com>
2020-02-17 17:27:13 +0900

    リファクタだうお

--- src/main/scala/com/github/cndjp/godfather/domain/validUrl/ValidUrl.scala
+++ src/main/scala/com/github/cndjp/godfather/domain/validUrl/ValidUrl.scala
@@ -5,15 +5,15 @@ import com.github.cndjp.godfather.exception.GodfatherException.GodfatherParseUrl
 import scala.util.Try
 
 // 文字列からURLの変換を安全に取り出す値クラスです
-case class ValidUrl(private val url: URL) {
+case class ValidUrl(protected val url: URL) {
   override def toString: String = this.url.toString
 }
 
 object ValidUrl {
 
   def from(maybeURL: String): Either[Throwable, ValidUrl] =
-    Try(ValidUrl(new URL(maybeURL))).toEither match {
+    Try(new URL(maybeURL)).toEither match {
       case Left(e)      => Left(GodfatherParseUrlException(e.getMessage))
-      case Right(value) => Right(value)
+      case Right(value) => Right(ValidUrl(value))
     }
 }
