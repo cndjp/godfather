79443edcf3feeb5ee5b49313f698497ce4a23abc
nnao45 <n4sekai5y@gmail.com>
2020-02-18 15:30:10 +0900

    リファクと

--- src/main/scala/com/github/cndjp/godfather/domain/validUrl/ValidUrl.scala
+++ src/main/scala/com/github/cndjp/godfather/domain/validUrl/ValidUrl.scala
@@ -5,10 +5,10 @@ import com.github.cndjp.godfather.exception.GodfatherException.GodfatherParseUrl
 import scala.util.Try
 
 // validUrlパッケージ内でしか参照できないので、ValidUrl#from経由でしかValidUrlをコンストラクトできなくします
-private[validUrl] case class RawUrl(value: URL) extends AnyVal
+private[validUrl] case class ValueUrl(value: URL) extends AnyVal
 
 // 文字列からURLの変換を安全に取り出す値クラスです
-case class ValidUrl(private val url: RawUrl) {
+case class ValidUrl(private val url: ValueUrl) {
   override def toString: String = this.url.value.toString
 }
 
@@ -17,6 +17,6 @@ object ValidUrl {
   def from(maybeURL: String): Either[Throwable, ValidUrl] =
     Try(new URL(maybeURL)).toEither match {
       case Left(e)      => Left(GodfatherParseUrlException(e.getMessage))
-      case Right(value) => Right(ValidUrl(RawUrl(value)))
+      case Right(value) => Right(ValidUrl(ValueUrl(value)))
     }
 }
